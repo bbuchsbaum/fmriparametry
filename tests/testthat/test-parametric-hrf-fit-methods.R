642ca7b196d07823434a0eb61d17c1faffc43ef5
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-02 07:24:29 -0400

    Regenerate NAMESPACE and use internal constructor

--- tests/testthat/test-parametric-hrf-fit-methods.R
+++ tests/testthat/test-parametric-hrf-fit-methods.R
@@ -5,7 +5,7 @@ context("parametric_hrf_fit S3 methods")
 
 pars <- matrix(rep(1:3, each = 2), nrow = 2, byrow = TRUE)
 amps <- c(1, 2)
-obj <- new_parametric_hrf_fit(
+obj <- fmriparametric:::new_parametric_hrf_fit(
   estimated_parameters = pars,
   amplitudes = amps,
   parameter_names = c("tau", "sigma", "rho"),
