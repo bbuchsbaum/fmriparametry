d2b892852d02b784883169236c073edf3158b152
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-09 13:31:05 -0400

    Enable OpenMP in fast convolution

--- src/fast_conv.cpp
+++ src/fast_conv.cpp
@@ -1,7 +1,11 @@
 #include <Rcpp.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 using namespace Rcpp;
 
+// [[Rcpp::plugins(openmp)]]
 // [[Rcpp::export]]
 NumericMatrix fast_batch_convolution_cpp(NumericVector signal, NumericMatrix kernels, int output_length) {
   int signal_length = signal.size();
@@ -13,6 +17,7 @@ NumericMatrix fast_batch_convolution_cpp(NumericVector signal, NumericMatrix ker
   // Simple nested loop implementation
   // This implements the convolution y[i] = sum_k x[i-k] * h[k]
   // where h is already reversed (as expected by stats::convolve with rev(kernel))
+  #pragma omp parallel for if(n_kernels > 1)
   for(int j = 0; j < n_kernels; ++j) {
     for(int i = 0; i < output_length; ++i) {
       double sum = 0.0;
